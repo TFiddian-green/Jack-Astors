964aded73dfcfafdf4a395b52feb8021d32fa813
Michael Song <kdeslauriers@bayviewglen.ca>
2023-05-26 14:58:53 -0400

    storyline and room

--- src/zork/CommandWords.java
+++ src/zork/CommandWords.java
@@ -2,7 +2,7 @@ package zork;
 
 public class CommandWords {
   // a constant array that holds all valid command words
-  private static final String validCommands[] = { "go", "quit", "help", "eat" ,"north", "east", "south", "west", "jump"};
+  private static final String validCommands[] = { "go", "quit", "help", "eat", "jump"};
 
   /**
    * Constructor - initialise the command words.
